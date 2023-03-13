32b958dadc554a69062dddb04661a8f9d158d6b5
Snezhana-star <cnega.2002@gmail.com>
2023-03-13 21:11:33 +0700

    111

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let eventBus = new Vue()
 Vue.component('columns', {
     template: `
-        <h2 class="error" v-for="error in errors">{{error}}</h2>
+       <h2 class="error" v-for="error in errors">{{error}}</h2>
         <div>
         <ul>
         <li v-for="card in column1"><p>{{card.title}}</p>
@@ -47,6 +47,98 @@ Vue.component('columns', {
             column3: [],
             errors: []
         }
+    },
+    mounted(){
+        eventBus.$on('card-submitted', card =>{
+            this.errors = []
+            if (this.column1.length < 3){
+                this.column1.push(card)
+            }
+            else {
+                this.errors.push("Сейчас нельзя добавить новую заметку")
+            }
+        })
+    },
+    methods: {
+        Status1(card, t){
+            t.completed = true
+            let count = 0
+            card.status = 0
+            this.errors = []
+            for (let i = 0; i < 5; i++) {
+                if (card.subtasks[i].title != null) {
+                    count++
+                }
+            }
+
+            for (let i = 0; i < count; i++) {
+                if (card.subtasks[i].completed === true) {
+                    card.status++
+                }
+            }
+            if (card.status/count*100 >= 50 && card.status/count*100 < 100 && this.column2.length < 5) {
+                this.column2.push(card)
+                this.column1.splice(this.column1.indexOf(card), 1)
+            } else if (this.column2.length === 5) {
+                this.errors.push('Сначала выполни задания из второй колонки')
+                if(this.column1.length > 0) {
+                    this.column1.forEach(item => {
+                        item.subtasks.forEach(item => {
+                            item.completed = true;
+                        })
+                    })
+                }
+            }
+        },
+        Status2(card, t) {
+            t.completed = true
+            let count = 0
+            card.status = 0
+            for (let i = 0; i < 5; i++) {
+                if (card.subtasks[i].title != null) {
+                    count++
+                }
+            }
+
+            for (let i = 0; i < count; i++) {
+                if (card.subtasks[i].completed === true) {
+                    card.status++
+                }
+            }
+            if (card.status / count * 100 === 100) {
+                this.column3.push(card)
+                this.column2.splice(this.column2.indexOf(card), 1)
+                card.date = new Date()
+            }
+        }
+    },
+    props: {
+        card: {
+            title: {
+                type: Text,
+                required: true
+            },
+            subtasks: {
+                type: Array,
+                required: true,
+                completed: {
+                    type: Boolean,
+                    required: true
+                }
+            },
+            date: {
+                type: Date,
+                required: false
+            },
+            status: {
+                type: Number,
+                required: true
+            },
+            errors: {
+                type: Array,
+                required: false
+            }
+        },
     },
 
 })
